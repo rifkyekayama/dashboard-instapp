582142027fa0930f7dc75bc2531518e77d0c69aa
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-01 02:36:20 +0700

    add secure asset

--- config/app.php
+++ config/app.php
@@ -109,6 +109,18 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Secure Asset
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    */
+    'asset' => env('APP_SECURE_ASSET'),
+
+    /*
+    |--------------------------------------------------------------------------
     | Logging Configuration
     |--------------------------------------------------------------------------
     |
