4794f7cdabe0d7fd23863a7c5a319c5868a04d83
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-03 06:10:04 +0700

    add lib imap

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Zalazdi\LaravelImap\Client;
+
 use App\Http\Requests;
 
 class homeController extends Controller
 {
     //
     public function index(){
-    	return view('pages.home')->withTitle('Dashboard');
+    	// return view('pages.home')->withTitle('Dashboard');
+
+    	$mails = new Client();
+    	// $mails->coonnect();
+    	dd($mails->connect()->getMailboxes());
     }
 }
