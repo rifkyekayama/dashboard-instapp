50aeb29a6b1008dc04079a9fa8a1963da9c8c198
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-08 06:01:40 +0700

    add middleware httpsProtocol

--- config/app.php
+++ config/app.php
@@ -191,7 +191,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
