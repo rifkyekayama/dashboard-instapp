baa8196326240a9089155732b722ac99e170201a
Rifky Ekayama <rifqykiw@gmail.com>
2016-09-08 05:37:51 +0700

    add primary key on model transaction

--- app/Models/Transaction/Transaction.php
+++ app/Models/Transaction/Transaction.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     //
+	protected $primaryKey = 'id_transaction';
+    
     public function customers(){
     	return $this->belongsTo('App\Models\Transaction\Customer', 'id_customer', 'customer_id');
     }
